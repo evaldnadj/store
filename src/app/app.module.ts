7cd71bf500927a94b53b7addbd0d4fdfbcad57d6
evaldnadj <e.nadj@yahoo.com>
2018-01-12 20:51:35 +0100

    1. zadatak

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { BuyersComponentComponent } from './components/buyers-component/buyers-component.component';
+import { ProductsComponentComponent } from './components/products-component/products-component.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+const appRoutes: Routes = [
+  { path: '', component: ProductsComponentComponent },
+  { path: 'buyers', component: BuyersComponentComponent },
+  { path: 'buyers/:id', component: BuyersComponentComponent },
+  { path: 'products', component: ProductsComponentComponent },
+  { path: 'products/:id', component: ProductsComponentComponent },
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LayoutComponent,
+    BuyersComponentComponent,
+    ProductsComponentComponent,
+    NavbarComponent 
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
